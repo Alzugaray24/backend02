186566bf70a294758df850c2dec51c5029780dbb
Matias Alzugaray Piven <alzugaray1997@gmail.com>
2024-04-12 22:04:55 -0300

    actualizando testing

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-import program from '../process.js';
+import dotenv from "dotenv";
+import program from "../process.js";
 
 dotenv.config();
 
@@ -8,25 +8,25 @@ const mode = program.opts().mode;
 let envFilePath;
 
 switch (mode) {
-    case 'prod':
-        envFilePath = "./src/config/.env.production";
-        break;
-    case 'dev':
-        envFilePath = "./src/config/.env.development";
-        break;
-    case 'test':
-        envFilePath = "./src/config/.env.testing";
-        break;
-    default:
-        throw new Error('Invalid mode specified');
+  case "prod":
+    envFilePath = "./src/config/.env.production";
+    break;
+  case "dev":
+    envFilePath = "./src/config/.env.development";
+    break;
+  case "test":
+    envFilePath = "./src/config/.env.testing";
+    break;
+  default:
+    throw new Error("Invalid mode specified");
 }
 
 dotenv.config({ path: envFilePath });
 
 export default {
-    port: process.env.PORT,
-    urlMongo: process.env.MONGO_URL,
-    adminName: process.env.ADMIN_NAME,
-    adminPassword: process.env.ADMIN_PASSWORD,
-    environment: mode
-}
+  port: process.env.PORT_CODE,
+  urlMongo: process.env.URL_MONGO,
+  adminName: process.env.ADMIN_NAME,
+  adminPassword: process.env.ADMIN_PASSWORD,
+  environment: mode,
+};
