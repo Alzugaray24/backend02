2e631a9ffb67ef4fc77fe23d20ac8e8c153125c9
Matias Alzugaray Piven <alzugaray1997@gmail.com>
2024-04-26 16:12:43 -0300

    Update cart.js

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -1,16 +1,8 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe(
-  process.env.APP_STRIPE_KEY ||
-    "pk_test_51P8O2ORurY0CPhSO3gGTSlaexZyzPjwu0iOQKBX7PoQ85OVagSdaCrfYED1a7DJRW3Bjt46qzmQuJ126qFmdmfvU00vYFQyuoP"
-);
-
-console.log(stripePromise);
-
 document.addEventListener("DOMContentLoaded", function () {
   const finalizeBtn = document.getElementById("finalizeBtn");
 
   finalizeBtn.addEventListener("click", async function () {
-    alert("jjsakds");
+    // Redireccionar a "/successPurchase"
+    window.location.href = "/successPurchase";
   });
 });
