b8bb2f087bad710640c7e3c3e5e611f41a7e386d
Matias Alzugaray Piven <alzugaray1997@gmail.com>
2024-04-26 12:31:39 -0300

    actualizando dev

--- src/app.js
+++ src/app.js
@@ -71,9 +71,7 @@ app.use("/api/extend/cart", cartExtendRouter.getRouter());
 
 app.use(errorHandler);
 
-const port = process.env.PORT || 3000;
-
-
+const port = process.env.PORT || 8080;
 
 app.listen(port, "0.0.0.0", console.log(`Server running on port ${port}`));
 
